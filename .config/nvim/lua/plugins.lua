d399a3aa32e3934701a7f83d89a15719a697b6e4
l0py2 <l0py2.git@gmail.com>
2023-06-10 14:29:45 +0100

    Packer added to Neovim

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,27 @@
+local function ensure_packer()
+	local fn = vim.fn
+
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+
+		vim.cmd [[packadd packer.nvim]]
+
+		return true
+	end
+
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+local packer = require('packer')
+
+packer.startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	if packer_bootstrap then
+		packer.sync()
+	end
+end)
