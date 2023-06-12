30b71c99f8e8850307fe100ebc3ce9396772478f
l0py2 <l0py2.git@gmail.com>
2023-06-12 13:10:00 +0100

    Updated nvim-cmp config to loop through and setup all the LSP servers

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,4 @@
-local cmp = require'cmp'
+local cmp = require('cmp')
 
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
@@ -9,3 +9,13 @@ cmp.setup({
 		{ name = 'nvim_lsp' }
 	})
 })
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lsp_servers = require('mason-lspconfig').get_installed_servers()
+
+for key, value in pairs(lsp_servers) do
+	require('lspconfig')[value].setup {
+		capabilities = capabilities
+	}
+end
